7f79cfebdffe69ab7f35641c2ec340c924a44e7d
Rebecca Mignone <Rebecca_mig15@rebeccas-mbp-2.home>
2014-06-28 21:44:46 -0400

    Initial Commit

---
+++ PicYak/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PicYak
+//
+//  Created by Rebecca Mignone on 6/28/14.
+//  Copyright (c) 2014 Mignone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PicYakTests/PicYakTests.m
@@ -0,0 +1,34 @@
+//
+//  PicYakTests.m
+//  PicYakTests
+//
+//  Created by Rebecca Mignone on 6/28/14.
+//  Copyright (c) 2014 Mignone. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PicYakTests : XCTestCase
+
+@end
+
+@implementation PicYakTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
